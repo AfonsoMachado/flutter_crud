d914d16d231c16fcb07044d2a9c965842670b89e
Afonso Machado <afonsosmachado@gmail.com>
2023-09-20 15:31:37 -0300

    create edit and remove user

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/data/dummy_users_dart.dart';
 import 'package:flutter_crud/models/user.dart';
@@ -17,5 +19,35 @@ class UsersProvider with ChangeNotifier {
 
   User byIndex(int i) {
     return _items.values.elementAt(i);
+  }
+
+  void put(User user) {
+    if (user == null) {
+      return;
+    }
+
+    if (user.id != null && _items.containsKey(user.id.toString())) {
+      _items.update(user.id.toString(), (value) => user);
+    } else {
+      final id = Random().nextInt(1000);
+      _items.putIfAbsent(
+          id.toString(),
+          () => User(
+                id: id,
+                name: user.name,
+                email: user.email,
+                avatarUrl: user.avatarUrl,
+              ));
+    }
+
+    // Notificar o provider das mudanças
+    notifyListeners();
+  }
+
+  void remove(User user) {
+    if (user != null && user.id != null) {
+      _items.remove(user.id.toString());
+      notifyListeners();
+    }
   }
 }
